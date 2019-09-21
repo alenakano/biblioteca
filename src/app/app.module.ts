f4e22d9c05b9aa154c3085d208bce860644fed6f
Alexandre <alex.nakano@gmail.com>
2019-09-21 11:21:10 -0300

    Angular flexlayout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { PesquisaComponent } from './pesquisa/pesquisa.component';
-import { NovaComponent } from './pesquisa/nova/nova.component';
+
 import { EmprestimoComponent } from './emprestimo/emprestimo.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { MaterialModule } from './material.module';
+import { NovaComponent } from './pesquisa/nova/nova.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -23,9 +25,10 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    AppRoutingModule ,
     BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule,
+    FlexLayoutModule,
     MaterialModule,
   ],
   providers: [],
