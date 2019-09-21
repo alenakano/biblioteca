0689e3394c589cee2c23a7aa0ed20f093f672d6e
Alexandre <alex.nakano@gmail.com>
2019-09-21 10:03:37 -0300

    Estrutura basica.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'pesquisa', component: PesquisaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { NovaComponent } from './pesquisa/nova/nova.component';
+import { EmprestimoComponent } from './emprestimo/emprestimo.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    PesquisaComponent,
+    NovaComponent,
+    EmprestimoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserAnimationsModule,
